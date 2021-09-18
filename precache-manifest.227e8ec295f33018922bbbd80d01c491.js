c8168446c8b79d3f35234e86716352adcc1e80ac
olga-alexandrovnaa <42916312+olga-alexandrovnaa@users.noreply.github.com>
2021-09-18 11:48:54 +0000

    Deploying to gh-pages from @ olga-alexandrovnaa/npo-lots@efec9821a9bc09c980aa38d51ce7ab6e27d132d7 🚀

---
+++ precache-manifest.227e8ec295f33018922bbbd80d01c491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edae2538ba81635ff18d2b5f33bb24a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e10a43a10ac1dbc07f2d",
+    "url": "./static/js/2.2bb11f7b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2bb11f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a244582800dd6483cb",
+    "url": "./static/js/main.d7b88ab3.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/npo-lots/precache-manifest.453687fcabb6292bb4f38877bf0e046f.js"
+  "./precache-manifest.227e8ec295f33018922bbbd80d01c491.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/npo-lots/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
